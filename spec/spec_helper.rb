f5549bf0ee7eb9ed2b365d393c5a60580a0e00bf
Federico Ramallo <framallo@gmail.com>
2015-08-31 12:27:51 -0500

    disable profiling

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -25,7 +25,7 @@ RSpec.configure do |config|
   config.run_all_when_everything_filtered = true
   config.disable_monkey_patching!
   config.default_formatter = 'doc' if config.files_to_run.one?
-  config.profile_examples = 10
+  # config.profile_examples = 10
   config.order = :random
   Kernel.srand config.seed
 end
