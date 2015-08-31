cccd5d934824e79d082b0fdb6bb0fd58147232dd
Federico Ramallo <framallo@gmail.com>
2015-08-31 10:53:04 -0500

    add rspec

---
+++ spec/spec_helper.rb
@@ -0,0 +1,31 @@
+require 'rubygems'
+require 'bundler/setup'
+require 'pry'
+require 'webmock/rspec'
+require 'aws_helper'
+require 'dotenv'
+Dotenv.load
+
+require 'bitnami'
+
+RSpec.configure do |config|
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  # rspec-mocks config goes here. You can use an alternate test double
+  # library (such as bogus or mocha) by changing the `mock_with` option here.
+  config.mock_with :rspec do |mocks|
+    # Prevents you from mocking or stubbing a method that does not exist on
+    # a real object. This is generally recommended, and will default to
+    # `true` in RSpec 4.
+    mocks.verify_partial_doubles = true
+  end
+  config.filter_run :focus
+  config.run_all_when_everything_filtered = true
+  config.disable_monkey_patching!
+  config.default_formatter = 'doc' if config.files_to_run.one?
+  config.profile_examples = 10
+  config.order = :random
+  Kernel.srand config.seed
+end
