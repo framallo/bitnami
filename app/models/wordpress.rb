9ce135cd4cdd1752a7f64426dce5be87b1e1e451
Federico Ramallo <framallo@gmail.com>
2015-09-07 18:30:45 -0500

    add tests to wordpress model

--- app/models/wordpress.rb
+++ app/models/wordpress.rb
@@ -1,4 +1,32 @@
+require 'bitnami'
+
 class Wordpress < ActiveRecord::Base
   belongs_to :user
   validates_presence_of :user
+
+  after_create :create_instance
+
+  def create_instance
+    CreateInstanceWorker.perform_async(id) if id
+  end
+
+  def actually_create_instance
+    return if instance_id
+    instance.create
+    self.instance_id = instance.id
+    save!
+  end
+
+  def fetch_status
+    WordpressStatusWorker.perform_async(id)
+  end
+
+  def actually_fetch_status
+    self.status = instance.status.state
+    save!
+  end
+
+  def instance
+    @instance ||= Bitnami::Wordpress.new(instance_id)
+  end
 end
--- spec/models/wordpress_spec.rb
+++ spec/models/wordpress_spec.rb
@@ -1,11 +1,51 @@
 require 'rails_helper'
 
 RSpec.describe Wordpress, type: :model do
+  let(:user) { create(:user) }
+  let(:subject) { described_class.new user: user }
+
+  before(:each) do
+    aws_stub_create
+  end
+
   it 'belongs to user ' do
     expect(subject).to belong_to :user
   end
 
   it 'requires a user' do
     expect(subject).to validate_presence_of(:user)
+  end
+
+  context '#create_instance' do
+    it 'creates a new instance after creation' do
+      allow_any_instance_of(described_class)
+        .to receive(:actually_create_instance)
+
+      expect(subject).to receive(:create_instance)
+        .and_call_original
+      subject.save!
+    end
+
+    it 'creates a new worker' do
+      expect do
+        subject.save!
+      end.to change(CreateInstanceWorker.jobs, :size).by(1)
+    end
+
+    it 'creates a new instance' do
+      subject.save!
+      CreateInstanceWorker.drain
+      subject.reload
+      puts subject.instance_id
+      expect(subject.instance_id).not_to be_nil
+    end
+  end
+
+  context '#actuall_create_instance' do
+    it 'actually creates an instance' do
+      allow(subject.instance).to receive(:create)
+        .and_call_original
+      subject.actually_create_instance
+    end
   end
 end
