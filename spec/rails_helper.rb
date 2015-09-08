964749f2278f84d410c215d847ecf990e660b478
Federico Ramallo <framallo@gmail.com>
2015-09-08 16:56:00 -0500

    allow codeclimate connections

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,6 +1,8 @@
 require 'rubygems'
 require 'simplecov'
 require 'codeclimate-test-reporter'
+require 'webmock'
+WebMock.disable_net_connect!(allow: 'codeclimate.com')
 CodeClimate::TestReporter.start
 SimpleCov.start
 
